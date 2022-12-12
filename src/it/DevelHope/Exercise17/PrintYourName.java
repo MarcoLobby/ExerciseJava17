d4fea2c59f636ae161819f7a873669fa4accd08d
Marco Lo Bello <xSalamander@outlook.it>
2022-12-12 20:24:53 +0100

    Initial Commit

---
+++ src/it/DevelHope/Exercise17/PrintYourName.java
@@ -0,0 +1,8 @@
+package it.DevelHope.Exercise17;
+
+public class PrintYourName {
+    public static void main(String[] args) {
+        String name = "Marco Lo Bello";
+        System.out.println(name);
+    }
+}
